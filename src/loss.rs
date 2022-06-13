108a90130ee2bee0c88af2b7d0a773564abfa500
LafeWessel <46427282+LafeWessel@users.noreply.github.com>
2022-06-13 12:40:36 -0500

    added loss derivatives

--- src/loss.rs
+++ src/loss.rs
@@ -1,20 +1,34 @@
 
 
 pub mod loss_fns{
+    use core::panic;
+    use std::cmp::Ordering;
+
 
     pub trait Loss{
-        fn calculate_loss(&self, predictions: &[f32], targets: &[f32]) -> f32;
+        fn loss(&self, predictions: &[f32], targets: &[f32]) -> f32;
+
+        fn loss_derivative(&self, predictions: &[f32], targets: &[f32]) -> Vec<f32>;
     }
 
     /// Mean Squared Error
     pub struct MSE{}
     impl Loss for MSE{
         /// Mean squared error
-        fn calculate_loss(&self, predictions: &[f32], targets: &[f32]) -> f32 {
+        fn loss(&self, predictions: &[f32], targets: &[f32]) -> f32 {
             predictions.iter()
                 .zip(targets.iter())
-                .map(|(p,r)| (p-r).powi(2))
+                .map(|(p,t)| (p-t).powi(2))
                 .sum::<f32>() / predictions.len() as f32
+        }
+
+        /// Derivative of mean squared error
+        fn loss_derivative(&self, predictions: &[f32], targets: &[f32]) -> Vec<f32> {
+            // 2 * (pred - targ) / targ.size
+            predictions.iter()
+                .zip(targets.iter())
+                .map(|(p,t)| 2.0 * (p-t) / predictions.len() as f32)
+                .collect()
         }
     }
 
@@ -22,11 +36,21 @@ pub mod loss_fns{
     pub struct MAE{}
     impl Loss for MAE{
         /// Mean absolute error
-        fn calculate_loss(&self, predictions: &[f32], targets: &[f32]) -> f32 {
+        fn loss(&self, predictions: &[f32], targets: &[f32]) -> f32 {
             predictions.iter()
                 .zip(targets.iter())
                 .map(|(p,r)| (p - r).abs())
                 .sum::<f32>() / predictions.len() as f32
+        }
+
+        /// Derivative of mean absolute error
+        fn loss_derivative(&self, predictions: &[f32], targets: &[f32]) -> Vec<f32> {
+            predictions.iter().zip(targets.iter()).map(|(p,t)| match p.partial_cmp(t) {
+                Some(Ordering::Greater) => 1.0,
+                Some(Ordering::Equal) => 0.0,
+                Some(Ordering::Less) => -1.0,
+                None => panic!("Cannot compare {}, {}", p, t)
+            }).collect()
         }
     }
 
@@ -42,7 +66,7 @@ mod tests{
         let a = MSE{};
         let pred = vec![0.0, 1.0, 2.0, 3.0];
         let resp = vec![2.0, 3.0, 4.0, 5.0];
-        assert_eq!(4f32, a.calculate_loss(&pred, &resp));
+        assert_eq!(4.0, a.loss(&pred, &resp));
     }
 
     #[test]
@@ -51,6 +75,25 @@ mod tests{
         let a = MAE{};
         let pred = vec![0.0, 1.0, 2.0, 3.0];
         let resp = vec![2.0, 3.0, 4.0, 5.0];
-        assert_eq!(2f32, a.calculate_loss(&pred, &resp));
+        assert_eq!(2.0, a.loss(&pred, &resp));
+    }
+
+    #[test]
+    /// Test MSE derivative
+    fn mse_derivative(){
+        let a = MSE{};
+        let pred = vec![0.0,1.0,2.0, 4.0];
+        let resp = vec![1.0, 2.0, 4.0, 4.0];
+        assert_eq!(vec![-0.5, -0.5, -1.0, 0.0], a.loss_derivative(&pred, &resp));
+    }
+
+    #[test]
+    /// Test MAE derivative
+    fn mae_derivative(){
+        let a = MAE{};
+        let pred = vec![0.0, 1.0, 2.0, 4.0];
+        let resp = vec![-1.0, 2.0, 3.0, 4.0];
+
+        assert_eq!(vec![1.0, -1.0, -1.0, 0.0], a.loss_derivative(&pred, &resp));
     }
 }
